5ab4f3d57295891a5ef7876b3b0ad7b4c9393879
furenku <furenku@gmail.com>
2019-08-01 22:07:56 -0500

    unstash

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+
+import { Switch, Route } from 'react-router-dom';
+
+import './App.scss';
+import MainHeader from './components/general/MainHeader/MainHeader';
+import Home from './components/Home/Home';
+import BooksList from './components/Books/BooksList/BooksList';
+import Page from './components/Page/Page';
+import BookDetail from './components/Books/BookDetail/BookDetail';
+import Logout from './components/general/Logout/Logout';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          
+      <MainHeader/>
+
+      <main>
+
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/libros" component={BooksList}/>
+          <Route exact path="/libro/:id" component={BookDetail}/>
+          
+          <Route exact path="/vision" render={
+            (props)=>{
+
+              return <Page title="Visión"/>
+              
+            }
+          }/>
+          
+          <Route exact path="/salir" component={Logout}/>
+
+        </Switch>
+
+      </main>
+
     </div>
   );
 }
